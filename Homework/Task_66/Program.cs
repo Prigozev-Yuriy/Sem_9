1f8225e6c65d74a386b1c3a6ae13c31f758cfa9d
Prigozev-Yuriy <138002979+Prigozev-Yuriy@users.noreply.github.com>
2023-07-25 14:17:41 +0300

    Task_66

---
+++ Homework/Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите число M: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int firstNum, int secondNum)
+
+{
+    if (firstNum < secondNum)
+    {
+        if (firstNum == secondNum) return firstNum;
+        return firstNum + SumNumbers(firstNum + 1, secondNum);
+    }
+    else if (firstNum == secondNum) return secondNum;
+    return secondNum + SumNumbers(firstNum, secondNum + 1);
+}
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от числа {firstNum} до числа {secondNum} равна: {SumNumbers(firstNum, secondNum)}");
